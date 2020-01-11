c4b3789a8ab3ea9f7baa22b59932940e5d823606
Alexander Neupokoev <admin@armavir.ru>
2020-01-11 20:07:40 +0300

    First work version scripts

---
+++ vk.py
@@ -0,0 +1,96 @@
+import requests
+
+import xkcd
+
+
+url_base = 'https://api.vk.com/method/'
+
+
+def get_url_for_upload_comics(token, group_id):
+    url = f'{url_base}photos.getWallUploadServer'
+    params_url = {
+        'access_token': token,
+        'v': '5.103',
+        'group_id': group_id
+    }
+
+    response = requests.get(url, params=params_url)
+    response.raise_for_status()
+    upload_url = response.json()['response']['upload_url']
+    album_id = response.json()['response']['album_id']
+    user_id = response.json()['response']['user_id']
+
+    return upload_url
+
+
+def upload_image_to_server_vk(upload_url, token, group_id):
+    payload = {
+        'access_token': token,
+        'v': '5.103',
+        'group_id': group_id
+    }
+
+    with open('comics.png', 'rb') as file:
+        photo = {
+            'photo': file
+        }
+        response = requests.post(upload_url, files=photo, json=payload)
+        response.raise_for_status()
+
+        if response.json()['photo']:
+            server_vk = response.json()['server']
+            photo_vk = response.json()['photo']
+            hash_vk = response.json()['hash']
+
+            return server_vk, photo_vk, hash_vk, token
+        else:
+            return "Ваш комикс не был загружен."
+
+
+def save_image_to_wall_vk(server_vk, photo_vk, hash_vk, token, group_id):
+    url = f'{url_base}photos.saveWallPhoto'
+    payload = {
+        'access_token': token,
+        'v': '5.103',
+        'group_id': group_id,
+        'server': server_vk,
+        'photo': photo_vk,
+        'hash': hash_vk
+    }
+
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    photo_id = response.json()['response'][0]['id']
+    owner_id = response.json()['response'][0]['owner_id']
+    return photo_id, owner_id
+
+
+def post_wall(photo_id, owner_id, message, token, group_id):
+    url = f'{url_base}wall.post'
+    attachment = f'photo{owner_id}_{photo_id}'
+    payload = {
+        'access_token': token,
+        'v': '5.103',
+        'owner_id': -(int(group_id)),
+        'attachments': attachment,
+        'from_group': 1,
+        'message': message
+    }
+
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    return response.json()['response']
+
+
+def posting_to_public(token, group_id):
+    message = xkcd.get_comics_from_xkcd()
+    upload_url = get_url_for_upload_comics(token, group_id)
+    server_vk, photo_vk, hash_vk, token = upload_image_to_server_vk(
+        upload_url, token, group_id)
+    photo_id, owner_id = save_image_to_wall_vk(
+        server_vk, photo_vk, hash_vk, token, group_id)
+    posted = post_wall(photo_id, owner_id, message, token, group_id)
+    if posted['post_id']:
+        return 'Ваш комикс опубликован'
+    else:
+        return 'Ваш комикс не опубликован'
---
+++ xkcd.py
@@ -0,0 +1,46 @@
+import requests
+import os
+import random
+
+url = 'https://xkcd.com/info.0.json'
+
+
+def get_last_num_comics():
+    response = requests.get(url)
+    response.raise_for_status()
+    num = response.json()['num']
+    return num
+
+
+def get_random_comics_url(num):
+    comics_num = random.randint(1, num)
+    url = f'http://xkcd.com/{comics_num}/info.0.json'
+
+    return url
+
+
+def get_image_extension(image_url):
+    extension = os.path.splitext(image_url)[1]
+    return extension
+
+
+def download_image_from_xkcd(image_url):
+    extension_image = get_image_extension(image_url)
+    filename = f'comics{extension_image}'
+
+    response = requests.get(image_url)
+    response.raise_for_status()
+
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+
+
+def get_comics_from_xkcd():
+    url = get_random_comics_url(get_last_num_comics())
+    response = requests.get(url)
+    response.raise_for_status()
+    image_url = response.json()['img']
+    comment_image = response.json()['alt']
+    download_image_from_xkcd(image_url)
+
+    return comment_image
